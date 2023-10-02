0d7a9446ad8d8701498be5a319bce5c0c43560ad
Mel Raposeiras <134176134+fraise-renard@users.noreply.github.com>
2023-10-01 21:48:03 -0300

    State machine update
    Now the game state is encapsulated into a state machine system.

---
+++ Uraken/gamestate.hpp
@@ -0,0 +1,56 @@
+/*
+ * menu.hpp
+ *
+ *  Created on: 9 de jun. de 2023
+ *      Author: hayde
+ */
+#pragma once
+#ifndef STATES_MENUSTATE_HPP_
+#define STATES_MENUSTATE_HPP_
+#include "gamedata.hpp"
+#include "State.hpp"
+#include "stdafx.hpp"
+#include "player.hpp"
+#include "fuel.hpp"
+#include "myContactListener.hpp"
+#include <SFML/Graphics.hpp>
+#include <SFML/Audio/Music.hpp>
+#include <typeinfo>
+#include <Box2D/Box2D.h>
+#include "b2_sf_converter.hpp"
+#include <vector>
+#include <iostream>
+#include "background.hpp"
+
+class GameState: public State {
+public:
+	GameState(GameDataRef data);
+	void init();
+	void update();
+	void draw();
+
+private:
+	GameDataRef data;
+	MyContactListener *contactListener;
+	sf::Music music;
+	sf::Vector2f vpos;
+	float dt = 0, elapsedtime = 0.3, winposy = 0;
+	sf::Clock clock;
+	sf::Sprite winscreen;
+	Fuel *fueldisplay;
+	Player *player;
+	Background *bg;
+	b2Body *ground;
+	b2World *world;
+	std::vector<b2Body*> platforms;
+	std::vector<sf::Sprite*> bamboos;
+	b2Body* createElement(int x, int y, int width, int height, b2BodyType type,
+			sf::Color color);
+	b2Body* createElement(int x, int y, int width, int height, b2BodyType type,
+			sf::Texture *tex, sf::IntRect frame);
+	sf::View view; //creation of the view to follow the player
+	bool freezeAll = false; //stops the simulation, game end
+
+};
+
+#endif /* STATES_MENUSTATE_HPP_ */
--- Uraken/main.cpp
+++ Uraken/main.cpp
@@ -4,12 +4,12 @@
  *  Created on: 18 de ago. de 2023
  *      Author: Grupo 8
  */
-#include "testbed.hpp"
+#include "stdafx.hpp"
+#include "application.hpp"
 #include <SFML/Graphics.hpp>
 int main(int argc, char **argv) {
 
-	Testbed space;
-	space.Run();
+	Application app(ww,wh);
 	return 0;
 }
 
